cf39e6285edc052e6b8c369ef78ddfcd9f0bcdd9
ayesharajkapoor <ayesharajkapoor@gmail.com>
2024-01-21 17:21:41 +0530

    simple output message

--- DemoProject/src/LoginScenarios/Login.java
+++ DemoProject/src/LoginScenarios/Login.java
@@ -70,6 +70,7 @@ public class Login {
 		WebElement loginButton = driver
 				.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
 		loginButton.click();
+		System.out.println("Demo PRoject");
 
 	}
 
